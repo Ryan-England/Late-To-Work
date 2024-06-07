a0f193c15c97583c42c6ce616dad42d3da3ca306
lweislak <150990328+lweislak@users.noreply.github.com>
2024-06-06 19:58:38 -0700

    Test branch commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-// Jim Whitehead
-// Created: 4/14/2024
-// Phaser: 3.70.0
-//
-// Cubey
-//
-// An example of putting sprites on the screen using Phaser
-// 
-// Art assets from Kenny Assets "Shape Characters" set:
-// https://kenney.nl/assets/shape-characters
+//CMPM120 - Final Game
+//Victoria Morgan and Lo Weislak
 
 // debug with extreme prejudice
 "use strict"
